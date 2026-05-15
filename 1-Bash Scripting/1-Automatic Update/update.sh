0fcf55bda7e765c2f7171e0283991dbc060d89d8
Luís Azevedo <luisazevedo2002work@gmail.com>
2026-05-15 12:29:12 +0000

    Add files via upload

---
+++ 1-Bash Scripting/1-Automatic Update/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Starting system update"
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt clean
+echo "System updated with success"
+
